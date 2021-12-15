0fece7d0ecd53c61c9997b06a52d807632acdb2a
Alieaes <u_mepia@naver.com>
2021-12-15 21:43:26 +0900

    Defs : 기본 LIB 사용 시 각 솔루션의 stdafx.h 에서 선언하여 사용하도록 함. do-while에서 사용할 IF_(TRUE/FALSE)_BREAK 선언

--- Shared_Defs.h
+++ Shared_Defs.h
@@ -1,6 +1,16 @@
+////////////////////////////////////////////////////
+// ifdef의 DEFINE 값들은 stdafx에서 선언 필요
 
 #ifdef USING_QTLIB
     #include <QString>
 #endif
 
+////////////////////////////////////////////////////
+
 #define NULLPTR nullptr
+
+#define IF_FALSE_BREAK(x) if( (x) == false ) break;
+#define IF_TRUE_BREAK(x) if( (x) == true ) break;
+
+#define IF_FALSE_CONTINUE(x) if( (x) == false ) continue;
+#define IF_TRUE_CONTINUE(x) if( (x) == true ) continue;
