7192cd154e7ca26b49e91201e1cac2793f7147a2
Alieaes <u_mepia@naver.com>
2021-12-19 22:34:25 +0900

    Singleton 주석 추가

--- Singleton.h
+++ Singleton.h
@@ -5,6 +5,12 @@
 
 #include "Shared_Defs.h"
 
+/*
+* Class T를 SingleTon으로 사용할 경우, 헤더파일의 전역으로 typedef를 설정
+* typedef Shared::Singletons::Singleton<T> typedefName;
+* 이후 GetInstance로 Instance를 가져옴
+*/
+
 namespace Shared
 {
     namespace Singletons
@@ -32,7 +38,6 @@ namespace Shared
         private:
             static T* _instance;
             static std::once_flag _flag;
-
         };
 
         template < typename T >
