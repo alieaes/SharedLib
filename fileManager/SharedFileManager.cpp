ca2d3da30aef97ed8563e0406ae093025ff965ae
Alieaes <u_mepia@naver.com>
2022-02-06 18:53:42 +0900

    추가 : 파일 입출력 관련 클래스 추가, 현재 로깅을 위한 용도로만 추가됨. 추후 추가 예정

---
+++ fileManager/SharedFileManager.cpp
@@ -0,0 +1,139 @@
+﻿#include "stdafx.h"
+
+#include "SharedFileManager.h"
+#include "SharedPath.h"
+#include "Shared_Defs.h"
+#include "string/SharedString.h"
+
+#include <thread>
+#include <fstream>
+
+#ifdef USING_QTLIB
+#include <qdatetime.h>
+#endif
+
+using namespace Shared::FileManager;
+
+FileMgr::FileMgr()
+{
+    _thMain = std::thread( AsyncFileWrite, this );
+}
+
+FileMgr::~FileMgr()
+{
+    _isStop = true;
+
+    _thMain.join();
+}
+
+bool FileMgr::InitFileMgr( FILEMGR fileMgr )
+{
+    bool isSuccess = false;
+    std::wstring sUnique;
+
+    do
+    {
+        if( fileMgr.sFileName.empty() == true || fileMgr.sFilePath.empty() == true || fileMgr.sUnique.empty() == true )
+            break;
+
+        if( fileMgr.sFileFullPath.empty() == true )
+            fileMgr.sFileFullPath = Shared::Path::ConvertSeparator( fileMgr.sFilePath + L"/" + fileMgr.sFileName + L".LOG" );
+
+        if( _mapFileMgr.count( fileMgr.sUnique ) > 0 )
+            break;
+
+        _mapFileMgr[ fileMgr.sUnique ] = fileMgr;
+
+        isSuccess = true;
+
+    } while( false );
+
+    return isSuccess;
+}
+
+void FileMgr::WriteFileContents( FILE_UNIQUE_NAME sUnique, std::wstring sContents )
+{
+    if( IsStop() == true )
+        return;
+
+    FILEMGR fileMgr = retrieveFileMgr( sUnique );
+
+    if( fileMgr.isValid() == false )
+        return;
+
+    FILEITEM item;
+
+    item.sUnique = sUnique;
+    item.sContents = sContents;
+
+    if( fileMgr.isUseAsync == true )
+    {
+        enQueueItem( item );
+    }
+    else
+    {
+        writeContents( sUnique, sContents );
+    }
+}
+
+FILEMGR FileMgr::retrieveFileMgr( FILE_UNIQUE_NAME sUnique )
+{
+    if( _mapFileMgr.count( sUnique ) == 0 )
+        return FILEMGR();
+
+    return _mapFileMgr[ sUnique ];
+}
+
+void FileMgr::AsyncFileWrite( LPVOID lpParam )
+{
+    FileMgr* cFileMgr = ( FileMgr* )lpParam;
+
+    while( cFileMgr->IsStop() == false )
+    {
+        FILEITEM item = cFileMgr->deQueueItem();
+
+        if( item.isValid() == true )
+            cFileMgr->writeContents( item.sUnique, item.sContents );
+
+        SleepEx( 10, false );
+    }
+}
+
+void FileMgr::enQueueItem( FILEITEM item )
+{
+    std::lock_guard< std::mutex > lock( _mutex );
+    _queue.push( item );
+}
+
+FILEITEM FileMgr::deQueueItem()
+{
+    std::lock_guard< std::mutex > lock( _mutex );
+    if( _queue.size() > 0 )
+    {
+        FILEITEM item = _queue.front();
+        _queue.pop();
+
+        return item;
+    }
+    else
+        return FILEITEM();
+}
+
+void FileMgr::writeContents( FILE_UNIQUE_NAME sUnique, std::wstring sContents )
+{
+    FILEMGR fileMgr = retrieveFileMgr( sUnique );
+
+    if( fileMgr.isValid() == false )
+        return;
+
+    std::ofstream ofs( fileMgr.sFileFullPath, std::ios_base::out | std::ios_base::app );
+
+    if( ofs.fail() == true )
+    {
+        //로깅
+    }
+
+    ofs << Shared::String::ws2s( sContents );
+
+    ofs.close();
+}
---
+++ fileManager/SharedFileManager.h
@@ -0,0 +1,89 @@
+﻿#ifndef __HDR_SHARED_FILEMGR__
+#define __HDR_SHARED_FILEMGR__
+#endif
+
+#include "Singleton.h"
+#include <string>
+#include <Windows.h>
+#include <queue>
+#include <mutex>
+
+#ifdef USING_QTLIB
+#include <qdatetime.h>
+#endif // USING_QTLIB
+
+namespace Shared
+{
+    namespace FileManager
+    {
+        typedef std::wstring FILE_UNIQUE_NAME;
+
+        typedef struct _FILEMGR
+        {
+            std::wstring       sUnique;
+
+            std::wstring       sFileName;
+            std::wstring       sFilePath;
+            std::wstring       sFileFullPath;
+
+            int                nMaxFileSize  = 50;
+            int                nMaxFileCnt   = 5;
+
+            bool               isUseCompress = false;
+            bool               isUseAsync    = false;
+
+            int                nMaxCompressFileCnt = 5;
+
+            bool isValid()
+            {
+                if( sUnique.empty() == false && sFileName.empty() == false && sFilePath.empty() == false && sFileFullPath.empty() == false )
+                    return true;
+                else
+                    return false;
+            }
+        } FILEMGR;
+
+        typedef struct _FILEITEM
+        {
+            FILE_UNIQUE_NAME   sUnique;
+            std::wstring       sContents;
+
+            bool isValid()
+            {
+                if( sUnique.empty() == false && sContents.empty() == false )
+                    return true;
+                else
+                    return false;
+            }
+        } FILEITEM;
+
+        class FileMgr
+        {
+        public:
+            FileMgr();
+            ~FileMgr();
+
+            bool                                               InitFileMgr( FILEMGR fileMgr );
+            void                                               WriteFileContents( FILE_UNIQUE_NAME sUnique, std::wstring sContents );
+            static void                                        AsyncFileWrite( LPVOID lpParam );
+
+            bool                                               IsStop() { return _isStop; }
+        private:
+            FILEMGR                                            retrieveFileMgr( FILE_UNIQUE_NAME sUnique );
+
+            void                                               writeContents( FILE_UNIQUE_NAME sUnique, std::wstring sContents );
+
+            void                                               enQueueItem( FILEITEM item );
+            FILEITEM                                           deQueueItem();
+
+            std::unordered_map< std::wstring, FILEMGR >        _mapFileMgr;
+            std::thread                                        _thMain;
+            std::queue< FILEITEM >                             _queue;
+            std::mutex                                         _mutex;
+
+            bool                                               _isStop     = false;
+        };
+    }
+}
+
+typedef Shared::Singletons::Singleton<Shared::FileManager::FileMgr> TyStFileMgr;
