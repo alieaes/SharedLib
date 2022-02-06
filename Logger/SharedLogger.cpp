7ba4195cfbedb93e3c416eec0470869ff2f70eaf
Alieaes <u_mepia@naver.com>
2022-02-06 18:52:41 +0900

    추가 : 파일 입출력 관련 클래스 추가, 현재 로깅을 위한 용도로만 추가됨. 추후 추가 예정

--- Logger/SharedLogger.cpp
+++ Logger/SharedLogger.cpp
@@ -1,17 +1,43 @@
 #include "stdafx.h"
 #include "SharedLogger.h"
 
+#include "fileManager/SharedFileManager.h"
+#include "utilities/SharedPath.h"
+#include "string/SharedString.h"
+
+#include "Shared_Defs.h"
+
 #ifdef USING_QTLIB
 #include <qdatetime.h>
 #endif
 
 using namespace Shared::Logger;
+using namespace Shared::FileManager;
+using namespace Shared::Path;
+
+void Log::init( TyLogger tyLogger )
+{
+    _isFileLogging = tyLogger.isFileLogging;
+
+    if( _isFileLogging == true )
+    {
+        FileMgr* stFileMgr = TyStFileMgr::GetInstance();
+
+        FILEMGR fileMgr;
+        fileMgr.sUnique = L"logger";
+        fileMgr.sFileName = L"A2I-SVC";
+        fileMgr.sFilePath = GetCurrentPath();
+        fileMgr.isUseAsync = true;
+
+        stFileMgr->InitFileMgr( fileMgr );
+    }
+}
 
 void Log::Debug( std::string LOGTYPE, const char* fileName, int codeLine, std::string format )
 {
 #ifdef USING_QTLIB
     QDateTime dtNow = QDateTime::currentDateTime();
-    QString sCurrentTime = dtNow.toString( "yyyy-MM-dd HH:mm:ss" );
+    QString sCurrentTime = dtNow.toString( DEFAULT_TIME_STRING );
     std::string msg = fmt::format( "[{}] [{}] ({}:{}) {} \n", sCurrentTime.toStdString(), LOGTYPE, strrchr( fileName, '\\' ) ? strrchr( fileName, '\\' ) + 1 : fileName, codeLine, format );
 #else
     time_t now = time( 0 );
@@ -23,6 +49,12 @@ void Log::Debug( std::string LOGTYPE, const char* fileName, int codeLine, std::s
 #endif // USING_QTLIB
 
     OutputDebugStringA( ( LPCSTR )msg.c_str() );
+
+    if( _isFileLogging )
+    {
+        FileMgr* stFileMgr = TyStFileMgr::GetInstance();
+        stFileMgr->WriteFileContents( L"logger", Shared::String::s2ws( msg ) );
+    }
 }
 
 void Log::Recode( const char* LOGTYPE, const char* fileName, int codeLine, std::string format )
