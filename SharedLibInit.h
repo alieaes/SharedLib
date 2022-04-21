b6ba4b70f5508db2e65e7110be85750e3597bd31
hogyun.yoon <hogyun.yoon@gytni.com>
2022-04-21 22:49:27 +0900

    수정 : SharedLibInit, 빌드오류 수정

--- SharedLibInit.h
+++ SharedLibInit.h
@@ -1,7 +1,11 @@
 #ifndef __HDR_INCLUDE_SHARED__
 #define __HDR_INCLUDE_SHARED__
 
+#include "stdafx.h"
+
 #ifdef USING_SHARED_LOGGER
     #include "SharedLogger.h"
     #define NAMESPACE_SHARED_LOGGER using namespace Shared::Logger;
+#endif
+
 #endif
