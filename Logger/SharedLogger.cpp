23f2823aa57db9eada98c83397fe28113c43bc7d
hogyun.yoon <hogyun.yoon@gytni.com>
2022-04-21 22:47:20 +0900

    추가 : Logger, 함수 오버라이딩 및 콘솔 출력 전용 함수 추가

--- Logger/SharedLogger.cpp
+++ Logger/SharedLogger.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "SharedLogger.h"
 
+#include <iostream>
+
 #include "fileManager/SharedFileManager.h"
 #include "utilities/SharedPath.h"
 #include "string/SharedString.h"
@@ -25,7 +27,7 @@ void Log::init( TyLogger tyLogger )
 
         FILEMGR fileMgr;
         fileMgr.sUnique = L"logger";
-        fileMgr.sFileName = L"A2I-SVC";
+        fileMgr.sFileName = L"SVC";
         fileMgr.sFilePath = GetCurrentPath();
         fileMgr.isUseAsync = true;
         fileMgr.isUseFileNumbering = true;
@@ -49,13 +51,48 @@ void Log::Debug( std::string LOGTYPE, const char* fileName, int codeLine, std::s
     std::string msg = fmt::format( "[{}] [{}] ({}:{}) {} \n", timeForm, LOGTYPE, strrchr( fileName, '\\' ) ? strrchr( fileName, '\\' ) + 1 : fileName, codeLine, format );
 #endif // USING_QTLIB
 
+    static DWORD dwNumberOfCharsWritten = 0;
     OutputDebugStringA( ( LPCSTR )msg.c_str() );
+    WriteConsoleA( GetStdHandle( STD_OUTPUT_HANDLE ), msg.c_str(), ( DWORD )msg.size(), &dwNumberOfCharsWritten, NULL );
 
     if( _isFileLogging )
     {
         FileMgr* stFileMgr = TyStFileMgr::GetInstance();
         stFileMgr->WriteFileContents( L"logger", Shared::String::s2ws( msg ) );
     }
+}
+
+void Log::Debug( std::string LOGTYPE, const char* fileName, int codeLine, std::wstring format )
+{
+#ifdef USING_QTLIB
+    QDateTime dtNow = QDateTime::currentDateTime();
+    QString sCurrentTime = dtNow.toString( DEFAULT_TIME_STRING );
+    std::wstring msg = fmt::format( L"[{}] [{}] ({}:{}) {} \n", sCurrentTime.toStdString(), LOGTYPE, strrchr( fileName, '\\' ) ? strrchr( fileName, '\\' ) + 1 : fileName, codeLine, format );
+#else
+    time_t now = time( 0 );
+    struct tm  tstruct;
+    char       timeForm[ 80 ];
+    tstruct = *localtime( &now );
+    strftime( timeForm, sizeof( timeForm ), "%Y-%m-%d %X", &tstruct ); // YYYY-MM-DD.HH:mm:ss
+    std::string msg = fmt::format( "[{}] [{}] ({}:{}) {} \n", timeForm, LOGTYPE, strrchr( fileName, '\\' ) ? strrchr( fileName, '\\' ) + 1 : fileName, codeLine, format );
+#endif // USING_QTLIB
+
+    static DWORD dwNumberOfCharsWritten = 0;
+    OutputDebugStringW( ( LPCWSTR )msg.c_str() );
+    WriteConsoleW( GetStdHandle( STD_OUTPUT_HANDLE ), msg.c_str(), ( DWORD )msg.size(), &dwNumberOfCharsWritten, NULL );
+
+    if( _isFileLogging )
+    {
+        FileMgr* stFileMgr = TyStFileMgr::GetInstance();
+        stFileMgr->WriteFileContents( L"logger", msg );
+    }
+}
+
+void Log::ConsolePrint( const char* fileName, int codeLine, std::wstring format )
+{
+    std::wstring msg = fmt::format( L"{}\n", format );
+    static DWORD dwNumberOfCharsWritten = 0;
+    WriteConsoleW( GetStdHandle( STD_OUTPUT_HANDLE ), msg.c_str(), ( DWORD )msg.size(), &dwNumberOfCharsWritten, NULL );
 }
 
 void Log::Recode( const char* LOGTYPE, const char* fileName, int codeLine, std::string format )
--- Logger/SharedLogger.h
+++ Logger/SharedLogger.h
@@ -2,6 +2,7 @@
 #define __HDR_SHARED_LOGGER__
 #endif
 
+#include "SharedLibInit.h"
 #include "Singleton.h"
 
 #include <formatter/format.h>
@@ -15,6 +16,9 @@
 
 #define LOGD(LOGTYPE, ...) \
     TyStLog::GetInstance()->Debug(LOGTYPE, __FILE__, __LINE__, fmt::format(__VA_ARGS__))
+
+#define CONSOLEP(...) \
+    TyStLog::GetInstance()->ConsolePrint( __FILE__, __LINE__, fmt::format(__VA_ARGS__))
 
 #define USE_LOGGER_INIT
 
@@ -36,7 +40,9 @@ namespace Shared
             static void              init( TyLogger tyLogger );
             // 디버그용 로그
             static void              Debug( std::string LOGTYPE, const char* fileName, int codeLine, std::string format );
+            static void              Debug( std::string LOGTYPE, const char* fileName, int codeLine, std::wstring format );
 
+            static void              ConsolePrint( const char* fileName, int codeLine, std::wstring format );
             // 파일 기록용 로그
             static void              Recode( const char* LOGTYPE, const char* fileName, int codeLine, std::string format );
 
