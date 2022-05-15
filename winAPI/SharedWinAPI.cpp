fc27eae118c8f03c1b52be8be13ae649cb34f557
Alieaes <Alieaes54@gmail.com>
2022-05-15 23:07:15 +0900

    추가 : Windows API 함수를 위한 파일 추가

---
+++ winAPI/SharedWinAPI.cpp
@@ -0,0 +1,30 @@
+﻿#include "SharedPath.h"
+#include "SharedString.h"
+
+#include "Shared_Defs.h"
+
+#include <filesystem>
+#include <algorithm>
+
+#ifdef USING_QTLIB
+#include <qdatetime.h>
+#include "SharedWinAPI.h"
+#endif
+
+namespace Shared
+{
+    namespace winAPI
+    {
+        std::wstring GetPCName()
+        {
+            wchar_t szTemp[ 1024 ] = { 0, };
+            DWORD dwSize = 255;
+
+            GetComputerName( szTemp, &dwSize );
+
+            return szTemp;
+        }
+    }
+}
+
+
---
+++ winAPI/SharedWinAPI.h
@@ -0,0 +1,20 @@
+﻿#ifndef __HDR_SHARED_PATH__
+#define __HDR_SHARED_PATH__
+#endif
+
+#include "SharedLibInit.h"
+
+#include <string>
+#include <Windows.h>
+
+#ifdef USING_QTLIB
+#include <qdatetime.h>
+#endif // USING_QTLIB
+
+namespace Shared
+{
+    namespace winAPI
+    {
+        std::wstring                 GetPCName();
+    }
+}
