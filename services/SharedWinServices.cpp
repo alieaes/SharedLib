7ca26a124f48027e9c8df625abcf3b634ec7248f
Alieaes <Alieaes54@gmail.com>
2022-06-13 23:41:09 +0900

    SharedWinServices : 기본 클래스 추가

---
+++ services/SharedWinServices.cpp
@@ -0,0 +1,41 @@
+﻿#include "SharedPath.h"
+#include "SharedString.h"
+
+#include "Shared_Defs.h"
+
+#include <filesystem>
+#include <algorithm>
+#include <QApplication>
+
+#ifdef USING_QTLIB
+#include <qdatetime.h>
+#include "SharedWinServices.h"
+#endif
+
+namespace Shared
+{
+    namespace services
+    {
+        bool IsExistService()
+        {
+            return true;
+        }
+
+        bool IsRunningService()
+        {
+            return true;
+        }
+
+        CService::CService()
+        {
+            scMgr = ::OpenSCManagerW( NULL, NULL, SC_MANAGER_ALL_ACCESS );
+
+            if( scMgr == NULL )
+            {
+                DWORD  dwError = ::GetLastError();
+            }
+        }
+    }
+}
+
+
---
+++ services/SharedWinServices.h
@@ -0,0 +1,30 @@
+﻿#ifndef __HDR_SHARED_PATH__
+#define __HDR_SHARED_PATH__
+#endif
+
+#include "SharedLibInit.h"
+
+#include <string>
+#include <Windows.h>
+
+#ifdef USING_QTLIB
+#include <qdatetime.h>
+#endif // USING_QTLIB
+
+namespace Shared
+{
+    namespace services
+    {
+        bool                         IsExistService();
+        bool                         IsRunningService();
+
+        class CService
+        {
+        public:
+            CService();
+
+        private:
+            SC_HANDLE                scMgr;
+        };
+    }
+}
